5e6057a197b664abf1861b0b5bba4e6aafb87028
CoderTh <CoderTh@outlook.com>
2021-12-19 17:22:27 +0800

    feat: Init go gateway project layout

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/CoderTH/go_gateway/router"
+	"github.com/e421083458/golang_common/lib"
+)
+
+func main() {
+	lib.InitModule("./conf/dev/", []string{"base", "mysql", "redis"})
+	defer lib.Destroy()
+	router.HttpServerRun()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	router.HttpServerStop()
+}
