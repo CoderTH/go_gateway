ef514f86f96d67acb2cc644e3323cd93bf01d8ef
CoderTh <CoderTh@outlook.com>
2021-12-21 18:05:55 +0800

    feat(service): Add service_info list api

---
+++ controller/service.go
@@ -0,0 +1,104 @@
+package controller
+
+import (
+	"fmt"
+
+	"github.com/CoderTH/go_gateway/dao"
+	"github.com/CoderTH/go_gateway/dto"
+	"github.com/CoderTH/go_gateway/middleware"
+	"github.com/CoderTH/go_gateway/public"
+	"github.com/e421083458/golang_common/lib"
+	"github.com/gin-gonic/gin"
+)
+
+type ServiceController struct{}
+
+func ServiceRegister(router *gin.RouterGroup) {
+	service := ServiceController{}
+	router.GET("/service_list", service.ServiceList)
+}
+
+// ServiceList godoc
+// @Summary 服务列表
+// @Description 服务列表
+// @Tags 服务管理
+// @ID /service/service_list
+// @Accept  json
+// @Produce  json
+// @Param info query string false "关键词"
+// @Param page_size query int true "每页个数"
+// @Param page_no query int true "当前页数"
+// @Success 200 {object} middleware.Response{data=dto.ServiceListOutput} "success"
+// @Router /service/service_list [get]
+func (service *ServiceController) ServiceList(c *gin.Context) {
+	params := &dto.ServiceListInput{}
+	if err := params.BindValidParam(c); err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	//从db中分页读取基本信息
+	serviceInfo := &dao.ServiceInfo{}
+	list, total, err := serviceInfo.PageList(c, tx, params)
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	//格式化输出信息
+	outList := []dto.ServiceListItemOutput{}
+	for _, listItem := range list {
+		serviceDetail, err := listItem.ServiceDetail(c, tx, &listItem)
+		if err != nil {
+			middleware.ResponseError(c, 2003, err)
+			return
+		}
+		//1、http后缀接入 clusterIP+clusterPort+path
+		//2、http域名接入 domain
+		//3、tcp、grpc接入 clusterIP+servicePort
+		serviceAddr := "unknow"
+		clusterIP := lib.GetStringConf("base.cluster.cluster_ip")
+		clusterPort := lib.GetStringConf("base.cluster.cluster_port")
+		clusterSSLPort := lib.GetStringConf("base.cluster.cluster_ssl_port")
+		if serviceDetail.Info.LoadType == public.LoadTypeHTTP &&
+			serviceDetail.HTTPRule.RuleType == public.HTTPRuleTypePrefixURL &&
+			serviceDetail.HTTPRule.NeedHttps == 1 {
+			serviceAddr = fmt.Sprintf("%s:%s%s", clusterIP, clusterSSLPort, serviceDetail.HTTPRule.Rule)
+		}
+		if serviceDetail.Info.LoadType == public.LoadTypeHTTP &&
+			serviceDetail.HTTPRule.RuleType == public.HTTPRuleTypePrefixURL &&
+			serviceDetail.HTTPRule.NeedHttps == 0 {
+			serviceAddr = fmt.Sprintf("%s:%s%s", clusterIP, clusterPort, serviceDetail.HTTPRule.Rule)
+		}
+		if serviceDetail.Info.LoadType == public.LoadTypeHTTP &&
+			serviceDetail.HTTPRule.RuleType == public.HTTPRuleTypeDomain {
+			serviceAddr = serviceDetail.HTTPRule.Rule
+		}
+		if serviceDetail.Info.LoadType == public.LoadTypeTCP {
+			serviceAddr = fmt.Sprintf("%s:%d", clusterIP, serviceDetail.TCPRule.Port)
+		}
+		if serviceDetail.Info.LoadType == public.LoadTypeGRPC {
+			serviceAddr = fmt.Sprintf("%s:%d", clusterIP, serviceDetail.GRPCRule.Port)
+		}
+		ipList := serviceDetail.LoadBalance.GetIPListByModel()
+		outItem := dto.ServiceListItemOutput{
+			ID:          listItem.ID,
+			LoadType:    listItem.LoadType,
+			ServiceName: listItem.ServiceName,
+			ServiceDesc: listItem.ServiceDesc,
+			ServiceAddr: serviceAddr,
+			Qps:         0,
+			Qpd:         0,
+			TotalNode:   len(ipList),
+		}
+		outList = append(outList, outItem)
+	}
+	out := &dto.ServiceListOutput{
+		Total: total,
+		List:  outList,
+	}
+	middleware.ResponseSuccess(c, out)
+}
--- public/const.go
+++ public/const.go
@@ -4,4 +4,21 @@ const (
 	ValidatorKey        = "ValidatorKey"
 	TranslatorKey       = "TranslatorKey"
 	AdminSessionInfoKey = "AdminSessionInfoKey"
+
+	LoadTypeHTTP = 0
+	LoadTypeTCP  = 1
+	LoadTypeGRPC = 2
+
+	HTTPRuleTypePrefixURL = 0
+	HTTPRuleTypeDomain    = 1
+
+	RedisFlowDayKey  = "flow_day_count"
+	RedisFlowHourKey = "flow_hour_count"
+
+	FlowTotal         = "flow_total"
+	FlowServicePrefix = "flow_service_"
+	FlowAppPrefix     = "flow_app_"
+
+	JwtSignKey = "my_sign_key"
+	JwtExpires = 60 * 60
 )
